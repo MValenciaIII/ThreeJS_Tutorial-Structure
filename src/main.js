f0a7474b931706d0ae71a89460ad3526d02ef2f4
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2025-04-15 10:44:18 -0500

    Initial Set Up code
    This code is the Inital set up needed for the first step of the World App Implementation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-// 1. Create an instance of the World app
-const world = new World(container);
+import { World } from "./World/World.js";
+//create the main function
+function main() {
+//code to set up the World App will go here
+//get a reference to the container element
+const container = document.querySelector('#scene-container')
+
+//1. create an instance of the World app
+const world = new World(container)
 
 //2. Render the scene
 world.render();
+}
+
+main();
